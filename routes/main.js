f795774e5cc1c8a7744bca801281f426fa3ab648
Gjarred Baker <gjarred23@gmail.com>
2023-06-18 18:04:31 -0500

    profile edit fix1

--- routes/main.js
+++ routes/main.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const upload = require("../middleware/multer");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/profileEdit", ensureAuth, postsController.getProfileEdit)
-router.post("/profileEdit", ensureAuth, postsController.postProfileEdit)
+router.post("/profileEdit", ensureAuth, upload.single("file"), postsController.postProfileEdit)
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
